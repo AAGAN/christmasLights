2fb4d6438fa974c1f2ac81b92951057c5ac90e2a
Arash Agan <arash.agan@gmail.com>
2020-12-05 22:48:40 -0500

    without rain drops

--- christmasTree2.ino
+++ christmasTree2.ino
@@ -675,7 +675,7 @@ const uint8_t gGradientPaletteCount =
 uint32_t first = millis();
   void loop()
 {
-  if(millis()-first < 680000){
+//  if(millis()-first < 680000){
     EVERY_N_SECONDS( SECONDS_PER_PALETTE ) {
     gCurrentPaletteNumber = addmod8( gCurrentPaletteNumber, 1, gGradientPaletteCount);
     gTargetPalette = gGradientPalettes[ gCurrentPaletteNumber ];
@@ -688,53 +688,53 @@ uint32_t first = millis();
   colorwaves( leds, NUM_LEDS, gCurrentPalette);
 
   FastLED.show();
-  FastLED.delay(20);
-  }
-  else if (millis()- first < 800000)
-  {
-       if (random(2) == 1) {
-      uint16_t i = random(Pixels);
-      if (redStates[i] < 1 && greenStates[i] < 1 && blueStates[i] < 1) {
-        redStates[i] = random(256);
-        greenStates[i] = random(256);
-        blueStates[i] = random(256);
-      }
-    }
-    
-    for(uint16_t l = 0; l < Pixels; l++) {
-      if (redStates[l] > 1 || greenStates[l] > 1 || blueStates[l] > 1) {
-        strip.setPixelColor(l, redStates[l], greenStates[l], blueStates[l]);
-        
-        if (redStates[l] > 1) {
-          redStates[l] = redStates[l] * fadeRate;
-        } else {
-          redStates[l] = 0;
-        }
-        
-        if (greenStates[l] > 1) {
-          greenStates[l] = greenStates[l] * fadeRate;
-        } else {
-          greenStates[l] = 0;
-        }
-        
-        if (blueStates[l] > 1) {
-          blueStates[l] = blueStates[l] * fadeRate;
-        } else {
-          blueStates[l] = 0;
-        }
-        
-      } else {
-        strip.setPixelColor(l, 0, 0, 0);
-      }
-    }
-    strip.show();
-    delay(1);
-  
-  }
-  else
-  {
-    first = millis();
-  }
+  FastLED.delay(5);
+//  }
+//  else if (millis()- first < 800000)
+//  {
+//       if (random(2) == 1) {
+//      uint16_t i = random(Pixels);
+//      if (redStates[i] < 1 && greenStates[i] < 1 && blueStates[i] < 1) {
+//        redStates[i] = random(256);
+//        greenStates[i] = random(256);
+//        blueStates[i] = random(256);
+//      }
+//    }
+//    
+//    for(uint16_t l = 0; l < Pixels; l++) {
+//      if (redStates[l] > 1 || greenStates[l] > 1 || blueStates[l] > 1) {
+//        strip.setPixelColor(l, redStates[l], greenStates[l], blueStates[l]);
+//        
+//        if (redStates[l] > 1) {
+//          redStates[l] = redStates[l] * fadeRate;
+//        } else {
+//          redStates[l] = 0;
+//        }
+//        
+//        if (greenStates[l] > 1) {
+//          greenStates[l] = greenStates[l] * fadeRate;
+//        } else {
+//          greenStates[l] = 0;
+//        }
+//        
+//        if (blueStates[l] > 1) {
+//          blueStates[l] = blueStates[l] * fadeRate;
+//        } else {
+//          blueStates[l] = 0;
+//        }
+//        
+//      } else {
+//        strip.setPixelColor(l, 0, 0, 0);
+//      }
+//    }
+//    strip.show();
+//    delay(1);
+//  
+//  }
+//  else
+//  {
+//    first = millis();
+//  }
 
   
   
